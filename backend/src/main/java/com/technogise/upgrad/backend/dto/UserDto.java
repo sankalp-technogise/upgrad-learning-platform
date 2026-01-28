12caaf27f1523c04dcf01afbba4267613697e3d2
Sankalp Yerawar <sankalp.yerawar@technogise.com>
2026-01-28 15:07:36 +0530

    feat: Implement OTP-based user authentication, including JWT generation, user management, and email services.

--- backend/src/main/java/com/technogise/upgrad/backend/dto/UserDto.java
+++ backend/src/main/java/com/technogise/upgrad/backend/dto/UserDto.java
@@ -3,4 +3,5 @@ package com.technogise.upgrad.backend.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.UUID;
 
-public record UserDto(@JsonProperty("id") UUID userId, String email) {}
+public record UserDto(@JsonProperty("id") UUID userId, String email) {
+}
