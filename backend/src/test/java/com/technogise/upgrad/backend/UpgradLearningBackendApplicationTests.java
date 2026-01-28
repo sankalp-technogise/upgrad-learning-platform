2deb511e81ed165cb46e22491d5f8dcdf3ec1e80
Sankalp Yerawar <sankalp.yerawar@technogise.com>
2026-01-28 17:26:34 +0530

    feat: Configure the test environment to use Testcontainers for database setup and activate the 'test' profile.

--- backend/src/test/java/com/technogise/upgrad/backend/UpgradLearningBackendApplicationTests.java
+++ backend/src/test/java/com/technogise/upgrad/backend/UpgradLearningBackendApplicationTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@org.springframework.context.annotation.Import(
-    com.technogise.upgrad.backend.config.TestConfig.class)
+@org.springframework.context.annotation.Import({
+  com.technogise.upgrad.backend.config.TestConfig.class,
+  com.technogise.upgrad.backend.config.TestContainersConfig.class
+})
+@org.springframework.test.context.ActiveProfiles("test")
 class UpgradLearningBackendApplicationTests {
 
   @Test
